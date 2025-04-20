f69b342cd447955b77ea42b83c1e353ecae95162
GaBizes <gabrieladossantosferreira97@gmail.com>
2025-04-20 16:19:30 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -9,15 +9,19 @@ int main() {
     float area1 = 769.700; 
     float pib1 = 1.29; 
     int pontosTuristicos1 = 15; 
-
-    // Dados da Carta 2 - Salvador
+    float densidadePopulacional1 = 137.89;
+    float pibcarpita1 = pib1 * 1000000000 / populacao1; 
+    
+    // Dados da Carta 2 - Salvador 
     char estado2[] = "BA"; 
     char codigo2[] = "A02"; 
     char nomeCidade2[] = "Salvador"; 
-    int populacao2 = 2.884000; 
+    int populacao2 = 2884000; 
     float area2 = 693.8; 
     float pib2 = 63.52; 
     int pontosTuristicos2 = 40; 
+    float densidadePopulacional2 = 3486.49;
+    float pibcarpita2 = pib2 * 1000000000 / populacao2; 
 
     printf("Carta 1: \n");
     printf("Estado: %s\n", estado1);
@@ -26,7 +30,9 @@ int main() {
     printf("População: %d\n", populacao1);
     printf("Área: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
-    printf("Número de Pontos Turísticos: %d\n\n", pontosTuristicos1);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional1);
+    printf("PIB per capita: %.2f reais\n\n", pibcarpita1);
 
     printf("Carta 2: \n");
     printf("Estado: %s\n", estado2);
@@ -36,6 +42,8 @@ int main() {
     printf("Área: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Número de Pontos Turísticos: %d\n", pontosTuristicos2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidadePopulacional2);
+    printf("PIB per capita: %.2f reais\n", pibcarpita2);
 
     return 0;
 }
